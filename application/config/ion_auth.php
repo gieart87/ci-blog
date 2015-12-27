eec3665b343a003f6314aeec88e028d8b25a34da
agie0925@gmail.com <agie0925@gmail.com>
2015-12-27 11:04:09 +0700

    email config

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -117,7 +117,7 @@ $config['identity_cookie_name'] = 'identity';
  | 	  'file' = Use the default CI config or use from a config file
  | 	  array  = Manually set your email config settings
  */
-$config['use_ci_email'] = FALSE; // Send Email using the builtin CI email class, if false it will return the code and the identity
+$config['use_ci_email'] = TRUE; // Send Email using the builtin CI email class, if false it will return the code and the identity
 $config['email_config'] = array(
 	'mailtype' => 'html',
 );
