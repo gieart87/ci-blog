8d92f88f57303c2dfcf12d912e6f6004435afe6c
agie0925@gmail.com <agie0925@gmail.com>
2016-01-03 19:18:58 +0700

    statcounter

--- application/views/themes/default/layout.php
+++ application/views/themes/default/layout.php
@@ -84,6 +84,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           $('[data-toggle="tooltip"]').tooltip();
       });
     </script>
-
+    
+    <!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10765102; 
+var sc_invisible=1; 
+var sc_security="6f05b87b"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify
+analytics" href="http://statcounter.com/shopify/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10765102/0/6f05b87b/1/"
+alt="shopify analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
   </body>
 </html>
