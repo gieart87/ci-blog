83dfb15672c4af10c829d4bbdd0336f4b7d116ef
agie0925@gmail.com <agie0925@gmail.com>
2016-07-17 22:25:46 +0700

    Fixed : User Profile Link

--- application/views/admin/default/parts/header.php
+++ application/views/admin/default/parts/header.php
@@ -33,7 +33,7 @@
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                        <a href="<?php echo site_url('admin/users/profile')?>" class="btn btn-default btn-flat">Profile</a>
+                                        <a href="<?php echo site_url('users/profile')?>" class="btn btn-default btn-flat">Profile</a>
                                     </div>
                                     <div class="pull-right">
                                         <a href="<?php echo site_url('signout')?>" class="btn btn-default btn-flat">Sign out</a>
